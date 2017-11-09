150ad3828b7d3e2a31028d82e1a7ecc39601a88c
Your Name <hashbyadamson@gmail.com>
2017-11-08 18:35:52 -0700

    Added Hannah Branch

---
+++ Project1/Project1/Controllers/ProgramController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1.Controllers
+{
+    public class ProgramController : Controller
+    {
+        // GET: Program
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProgramView(string proName, string internName, string sfccAddress, string ageGroup, string weekdayTime, string proDesc)
+        {
+            ViewBag.ProgramName = proName;
+            ViewBag.InternName = internName;
+            ViewBag.SFCCAddress = sfccAddress;
+            ViewBag.AgeGroup = ageGroup;
+            ViewBag.WeekdayTime = weekdayTime;
+            ViewBag.ProgDescription = proDesc;
+
+
+            return View();
+        }
+    }
+}
